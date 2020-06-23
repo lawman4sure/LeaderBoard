35f024e98d9399ff86a259ea6c8a5fb1e7d566ef
lawman4sure <lawrenceobioma617@gmail.com>
2020-06-22 22:16:48 -0700

    a succesful commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const button = document.querySelector("button");
+
+button.addEventListener("click", runEvent);
+
+function runEvent (e){
+    console.log("click works");
+}
+
+ 
+       let pointsArray = [];
+      json.forEach(items =>{
+          pointsArray = [...items]
+      })
+        
+    //let obj = {}
+    //obj.row = pointsArray;
+   // console.log(obj);
